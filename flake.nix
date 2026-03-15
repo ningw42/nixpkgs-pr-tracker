29386da26cf8fb7acb4a68c07daee7c04074be6e
Ning Wang <unidentifiedme26@gmail.com>
2026-03-15 11:46:35 +0800

    build(flake): make dependencies follow the `nixpkgs` input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
